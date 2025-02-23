8b29adbe50a7892c0720525f8646f33ac62ad064
Muhammad540 <thisisdeahmed@gmail.com>
2025-02-23 23:05:53 +0300

    loss computation added to engine

--- engine/model_utils/engine.py
+++ engine/model_utils/engine.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from .transformer import BartEncoder, BartDecoder
 
 class BART(nn.Module):
@@ -13,13 +14,20 @@ class BART(nn.Module):
         
         self.encoder = BartEncoder(config)
         self.decoder = BartDecoder(config)
+        # Tie the decoder embedding and output projection layer weights 
+        # Reason ? 
+        # 1. Reduce model params
+        # 2. Act as a regularizer 
+        # 3. Since it is just an inverse operation (embedding: input tokens -> embedding, projection: embedding -> output tokens)
+        self.decoder.output_projection.weight = self.decoder.embedding.embedding.weight
         
     def forward(self,
                 input_ids,
                 decoder_input_ids,
                 encoder_padding_mask=None,
                 decoder_padding_mask=None,
-                decoder_causal_mask=None):
+                decoder_causal_mask=None,
+                labels=None):
         """
         Args:
             input_ids: (batch_size, seq_len)
@@ -27,6 +35,10 @@ class BART(nn.Module):
             encoder_padding_mask: (batch_size, seq_len)
             decoder_padding_mask: (batch_size, seq_len)
             decoder_causal_mask: (batch_size, seq_len, seq_len)
+            labels: (batch_size, seq_len)
+        Returns:
+            (outputs, loss) if labels are provided else (output tokens)
+            outputs: (batch_size, seq_len, vocab_size)
         """
         encoder_output = self.encoder(input_ids, 
                                       attention_mask=encoder_padding_mask)
@@ -34,4 +46,15 @@ class BART(nn.Module):
                                       encoder_output=encoder_output,
                                       self_attention_mask=decoder_causal_mask,
                                       cross_attention_mask=decoder_padding_mask)
+        
+        if labels is not None:
+            loss = F.cross_entropy(
+                # (batch_size * seq_len, vocab_size)
+                decoder_output.view(-1, decoder_output.size(-1)),
+                # (batch_size * seq_len)
+                labels.view(-1),
+                ignore_index=-100
+            )
+            return decoder_output, loss
+        
         return decoder_output
