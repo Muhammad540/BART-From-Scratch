ebc5ac62c8c8a51396c3e21a044b92def2660376
Muhammad540 <thisisdeahmed@gmail.com>
2025-03-01 21:35:40 +0300

    add tokenizer + curate dataset

--- engine/data_utils/dataset_loading.py
+++ engine/data_utils/dataset_loading.py
@@ -1,13 +1,14 @@
 from datasets import load_dataset 
 import torch 
 from torch.utils.data import Dataset, DataLoader
+from transformers import BartTokenizer
 
 class SummarizationDataset(Dataset):
     def __init__(self,
                  articles,
                  summaries,
-                 tokenizer,
-                 max_length):
+                 tokenizer=BartTokenizer.from_pretrained("facebook/bart-base"),
+                 max_length=512):
         self.articles = articles
         self.summaries = summaries 
         self.tokenizer = tokenizer 
@@ -30,7 +31,9 @@ class SummarizationDataset(Dataset):
                                            padding="max_length",
                                            truncation=True)
         
+        # this masking is needed to distinguish between padding and actual tokens
         article_mask = article_encodings["attention_mask"]
+        summary_mask = summary_encodings["attention_mask"]
         
         # perform teacher forcing 
         # decoder input ids are what we feed into the decoder 
@@ -48,6 +51,37 @@ class SummarizationDataset(Dataset):
         return {
             "input_ids": torch.tensor(article_encodings["input_ids"]),
             "attention_mask": torch.tensor(article_mask),
-            "decoder_input_ids": torch.tensor(summary_encodings["input_ids"][:,:-1]),
-            "labels": torch.tensor(summary_encodings["input_ids"][:,1:])
+            "decoder_input_ids": torch.tensor(summary_encodings["input_ids"][:-1]),
+            "decoder_attention_mask": torch.tensor(summary_mask[:-1]),
+            "labels": torch.tensor(summary_encodings["input_ids"][1:])
         }
+
+
+def load_cnn_dailymail(batch_size,
+                        max_length):
+    dataset = load_dataset("abisee/cnn_dailymail", "3.0.0")
+    
+    tokenizer = BartTokenizer.from_pretrained("facebook/bart-base")
+    
+    train_dataset = SummarizationDataset(
+        articles=dataset["train"]["article"],
+        summaries=dataset["train"]["highlights"],
+        tokenizer=tokenizer,
+        max_length=max_length)
+    val_dataset = SummarizationDataset(
+        articles=dataset["validation"]["article"],
+        summaries=dataset["validation"]["highlights"],
+        tokenizer=tokenizer,
+        max_length=max_length)
+    
+    test_dataset = SummarizationDataset(
+        articles=dataset["test"]["article"],
+        summaries=dataset["test"]["highlights"],
+        tokenizer=tokenizer,
+        max_length=max_length)
+    
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    
+    return train_dataloader, val_dataloader, test_dataloader
